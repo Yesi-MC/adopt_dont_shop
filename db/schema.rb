813768a9fc607077f036873f32d646c6938b4d4e
Yesi Meza <yesi.meza10@gmail.com>
2020-12-14 00:22:42 -0700

    added app default value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_075214) do
+ActiveRecord::Schema.define(version: 2020_12_14_070254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2020_12_13_075214) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
-    t.string "application_status"
+    t.string "application_status", default: "In Progress"
   end
 
   create_table "pet_applications", force: :cascade do |t|
