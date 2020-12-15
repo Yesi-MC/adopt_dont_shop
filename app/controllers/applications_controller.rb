1e5aa804cf63d61a43efdbb58724a8e16cb0b9e3
Yesi Meza <yesi.meza10@gmail.com>
2020-12-14 18:18:56 -0700

    add flash message

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -16,9 +16,11 @@ class ApplicationsController < ApplicationController
     zip: params[:zip],
     description: params[:description]
     })
-    application.save
-    # require 'pry'; binding.pry
-
-    redirect_to "/applications/#{application.id}"
+    if application.save
+      redirect_to "/applications/#{application.id}"
+    else 
+      flash[:notice] = "Please fill in all fields"
+      render :new
+    end 
   end
-end
+endgit 
